9452c9186100e89d2b25cf84ba26eca7a9970196
Oscar CENTENO MORA <68450831+OskyRosky@users.noreply.github.com>
2024-12-19 09:22:09 -0600

    Create 08-conditionals.js
    Conditionals

---
+++ JS files/08-conditionals.js
@@ -0,0 +1,97 @@
+/*
+Clase 23 - Condicionales
+Vídeo: https://youtu.be/1glVfFxj8a4?t=7277
+*/
+
+// if, else if, else
+
+// if (si)
+
+let age = 37
+
+if (age == 37) {
+    console.log("La edad es 37")
+}
+
+// else (si no)
+
+if (age == 37) {
+    console.log("La edad es 37")
+} else {
+    console.log("La edad no es 37")
+}
+
+// else if (si no, si)
+
+if (age == 37) {
+    console.log("La edad es 37")
+} else if (age < 18) {
+    console.log("Es menor de edad")
+} else {
+    console.log("La edad no es 37 ni es menor de edad")
+}
+
+// Operador ternario
+
+const message = age == 37 ? "La edad es 37" : "La edad no es 37"
+console.log(message)
+
+// switch
+
+let day = 3
+let dayName
+
+switch (day) {
+    case 0:
+        dayName = "Lunes"
+        break
+    case 1:
+        dayName = "Martes"
+        break
+    case 2:
+        dayName = "Miércoles"
+        break
+    case 3:
+        dayName = "Jueves"
+        break
+    case 4:
+        dayName = "Viernes"
+        break
+    case 5:
+        dayName = "Sábado"
+        break
+    case 6:
+        dayName = "Domingo"
+        break
+    default:
+        dayName = "Número de día incorrecto"
+}
+
+console.log(dayName)
+
+
+
+//---------
+
+let age = 18;
+let day = 5;
+
+// Combinando if, else y switch
+if (age < 18) {
+    console.log("Es menor de edad");
+} else if (age >= 18 && age < 30) {
+    console.log("Es un adulto joven");
+} else {
+    console.log("Es un adulto");
+}
+
+switch (day) {
+    case 0:
+        console.log("Hoy es Lunes");
+        break;
+    case 5:
+        console.log("Hoy es Sábado");
+        break;
+    default:
+        console.log("Es un día entre semana o Domingo");
+}
